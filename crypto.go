eebb727ad82c582175a4a6309e5030136c6a80cf
Andrey Borodin <amborodin@acm.org>
2017-09-07 13:46:47 +0500

    backup encryption prototype

---
+++ crypto.go
@@ -0,0 +1,91 @@
+package walg
+
+import (
+	"os"
+	"golang.org/x/crypto/openpgp"
+	"os/exec"
+	"bytes"
+	"io"
+)
+
+type Crypter struct {
+	configured, armed bool
+	keyRingId         string
+
+	pubKey    openpgp.EntityList
+	secretKey openpgp.EntityList
+}
+
+func (crypter *Crypter) IsUsed() bool {
+	if !crypter.configured {
+		crypter.Configure()
+	}
+	return crypter.armed
+}
+
+func (crypter *Crypter) Configure() {
+	crypter.configured = true
+	crypter.keyRingId = GetKeyRingId()
+	crypter.armed = len(crypter.keyRingId) != 0
+}
+
+func (crypter *Crypter) Encrypt(writer io.WriteCloser) (io.WriteCloser, error) {
+	if crypter.pubKey == nil {
+		armour, err := GetPubRingArmour(crypter.keyRingId)
+		if err != nil {
+			return nil, err;
+		}
+
+		entitylist, err := openpgp.ReadArmoredKeyRing(bytes.NewReader(armour))
+		if err != nil {
+			return nil, err
+		}
+		crypter.pubKey = entitylist
+	}
+
+	var wc, err0 = openpgp.Encrypt(writer,crypter.pubKey,nil,nil,nil)
+	if err0 != nil {
+		return nil, err0;
+	}
+
+	return wc, nil
+}
+
+func (crypter *Crypter) Decrypt() (error) {
+	if crypter.pubKey == nil {
+		armour, err := GetSecretRingArmour(crypter.keyRingId)
+		if err != nil {
+			return err;
+		}
+
+		entitylist, err := openpgp.ReadArmoredKeyRing(bytes.NewReader(armour))
+		if err != nil {
+			return err
+		}
+		crypter.pubKey = entitylist
+	}
+
+	return nil
+}
+
+func GetKeyRingId() string {
+	return os.Getenv("WALE_GPG_KEY_ID")
+}
+
+const gpgBin = "/usr/local/bin/gpg"
+
+func GetPubRingArmour(keyId string) ([]byte, error) {
+	out, err := exec.Command(gpgBin, "-a", "--export", "-r", "\""+keyId+"\"").Output()
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func GetSecretRingArmour(keyId string) ([]byte, error) {
+	out, err := exec.Command(gpgBin, "-a", "--export-secret-key", "-r", "\""+keyId+"\"").Output()
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
---
+++ test_tools/cmd/crypt/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/wal-g/wal-g"
+)
+
+type ZeroWriter struct {
+
+}
+
+func (b *ZeroWriter) Write(p []byte) (n int, err error) {
+	return len(p),nil
+}
+
+
+func main() {
+	id := walg.GetKeyRingId()
+	fmt.Printf("Keyring ID: %v\n", id)
+	var armour, err = walg.GetPubRingArmour(id)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("Pubkey armour: %v\n", string(armour))
+
+	armour, err = walg.GetSecretRingArmour(id)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("Secret armour: %v\n", string(armour))
+
+	var c walg.Crypter
+	err = c.Encrypt(nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	err = c.Decrypt()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
